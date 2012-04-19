303d26203c9dfeb8692861a5032fbe19d9b8aec0
Doug Rand <drand@mitre.org>
2012-04-19 14:14:00 -0400

    Cleanup the poms, add needed javascript required support files.

--- simple-db-repository/src/main/java/org/mitre/openid/connect/repository/db/model/Role.java
+++ simple-db-repository/src/main/java/org/mitre/openid/connect/repository/db/model/Role.java
@@ -24,7 +24,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
