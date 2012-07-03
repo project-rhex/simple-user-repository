e56327f910fea7c199a5a7c350322c089d8e3e5c
Doug Rand <drand@mitre.org>
2012-07-03 15:28:53 -0400

    Clean up user table display (put edit icon in it's own column).

--- simple-db-repository/src/main/webapp/resources/js/manage_users.js
+++ simple-db-repository/src/main/webapp/resources/js/manage_users.js
@@ -60,8 +60,9 @@ users.UserList = Backbone.Collection.extend({
 	
 users.UserView = Backbone.View.extend({
 	tagName: 'tr',
-	template: _.template('<td><%= userId %></td><td><a href="./users/editUser/<%= extendedProperties._USER_ID %>' +
-	'"><%= givenName %>&nbsp;<img height="20" width="20" src="./resources/images/edit.png"></a></td>' +
+	template: _.template('<td><%= userId %></td>' +
+	'<td><a href="./users/editUser/<%= extendedProperties._USER_ID %>"><%= givenName %></a></td>' + 
+	'<td><a href="./users/editUser/<%= extendedProperties._USER_ID %>"><img height="20" width="20" src="./resources/images/edit.png"></a></td>' +
 	'<td><%= familyName %></td><td><%= extendedProperties.TITLE %></td><td><%= email %></td>' +
 	'<td>unknown</td><td><a href="#" data-user-id="<%= extendedProperties._USER_ID %>">' +
 	'<img height="20" width="20" src="./resources/images/delete.png"></a></td>' ),
@@ -91,7 +92,7 @@ users.UserListView = Backbone.View.extend({
     this.$el.append(userView.el);
   },
   render: function(){
-	$(this.el).append("<tr><th>Username</th><th>First Name</th><th>Last Name</th><th>Title</th><th>Email</th><th>Last Login</th><th></th></tr>");
+	$(this.el).append("<tr><th>Username</th><th>First Name</th><th></th><th>Last Name</th><th>Title</th><th>Email</th><th>Last Login</th><th></th></tr>");
     this.collection.forEach(this.addOne, this);
   }
 });
